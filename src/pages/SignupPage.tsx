48265395856133e57cdfd7cd57309d76b4b0aff8
김철우 <chul8747@naver.com>
2024-03-07 19:58:07 +0900

    fix :: 불필요한 태그 삭제

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,13 +1,10 @@
 import Signup from "@/components/signup"
-import { useState } from "react"
 
 
 
 const SignupPage = () => {
     return (
-        <>
         <Signup />
-        </>
     )
 }
 export default SignupPage
