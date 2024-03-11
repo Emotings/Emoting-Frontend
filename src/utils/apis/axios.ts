de88ef6e179d82d777db1c89cf9ba76fdebb4a68
김철우 <chul8747@naver.com>
2024-03-11 10:11:08 +0900

    feat :: 자동 토큰 추가를 위한 interceptor request 생성

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -1,6 +1,21 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { Cookies } from "react-cookie";
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
 });
+
+const cookies = new Cookies();
+
+instance.interceptors.request.use(
+	(config) => {
+		const accessToken = cookies.get('access_token');
+		const returnConfig = { ...config };
+		if (accessToken) {
+			returnConfig.headers!['Authorization'] = `Bearer ${accessToken}`;
+		}
+		return returnConfig;
+	},
+	(error: AxiosError) => Promise.reject(error)
+);
