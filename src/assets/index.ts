3177136a46344d68d2810dcd12cfc58d6b8fef8c
jikwan <jikwan0327@gmail.com>
2024-03-16 23:07:45 +0900

    feat: svgr 설치 및 이모지 생성 페이지 svg 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
 export { default as Google } from "./Google.svg";
 export { default as KakaoTalk } from "./KakaoTalk.svg";
 export { default as Naver } from "./Naver.svg";
-export {default as Profile} from "./Profile.svg"
-export {default as PlusPhoto} from "./PlusPhoto.svg"
+export { default as Profile } from "./Profile.svg";
+export { default as PlusPhoto } from "./PlusPhoto.svg";
